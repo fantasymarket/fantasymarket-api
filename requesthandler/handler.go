137f23ebf3f8559c103b72a120ae88fc4410abff
Arthlight <arthurpbh@gmail.com>
2020-02-23 16:25:56 +0100

    first commit to save the files

---
+++ requesthandler/handler.go
@@ -0,0 +1,19 @@
+package requesthandler
+
+import (
+	"fmt"
+	"github.com/go-chi/chi"
+	"net/http"
+)
+
+func GetPortfolioNumbers() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Alex = Gay"))
+	}
+}
+
+func GetStockRequest(w http.ResponseWriter, r *http.Request) {
+	stock_name := chi.URLParam(r, "name")
+
+
+}
