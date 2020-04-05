5f3daee1f908260a07ded14475b25b165b9abe41
exploingcamera <mail@henrygressmann.de>
2020-04-05 19:27:52 +0200

    refactor: (now actually) move api/handler.go to own v1 package, start versioning the api
    Signed-off-by: exploingcamera <mail@henrygressmann.de>

---
+++ api/v1/stocks.go
@@ -0,0 +1,53 @@
+package v1
+
+import (
+	"fantasymarket/game/stocks"
+	"fantasymarket/utils/http/responses"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"gopkg.in/yaml.v3"
+)
+
+func (api *APIHandler) getAllStocks(w http.ResponseWriter, r *http.Request) {
+	allStocks, err := ioutil.ReadFile("game/stocks.yaml")
+	if err != nil {
+		responses.ErrorResponse(w, http.StatusInternalServerError, "error getting list of stocks")
+		return
+	}
+
+	m := []stocks.StockDetails{} // Test again if this works, I checked the docs and in theory when the map is already initialized it should still work. If not change it back again
+	err = yaml.Unmarshal(allStocks, &m)
+
+	if err != nil {
+		responses.ErrorResponse(w, http.StatusInternalServerError, "error parsing stocks")
+		return
+	}
+
+	responses.CustomResponse(w, m, 200)
+}
+
+func (api *APIHandler) getStockDetails(w http.ResponseWriter, r *http.Request) {
+	symbol := chi.URLParam(r, "symbol")
+	yamlData, err := ioutil.ReadFile("game/stocks.yaml")
+
+	if err != nil {
+		responses.ErrorResponse(w, http.StatusInternalServerError, "error getting list of stocks")
+	}
+
+	var stocks []stocks.StockDetails
+
+	if err := yaml.Unmarshal(yamlData, &stocks); err != nil {
+		responses.ErrorResponse(w, http.StatusInternalServerError, "error parsing stock")
+	}
+
+	for i := range stocks {
+		if stocks[i].Symbol == symbol {
+			responses.CustomResponse(w, stocks[i], 200)
+			return
+		}
+	}
+
+	responses.ErrorResponse(w, http.StatusNotFound, "no stock with symbol available")
+}
---
+++ api/v1/v1.go
@@ -0,0 +1,63 @@
+package v1
+
+import (
+	"fantasymarket/database"
+	"fantasymarket/game"
+	"net/http"
+
+	"github.com/go-chi/chi"
+)
+
+// APIHandler holds the dependencies for http handlers
+type APIHandler struct {
+	DB   *database.Service
+	Game *game.Service
+}
+
+// NewAPIRouter creates a new API HTTP handler
+func NewAPIRouter(db *database.Service, game *game.Service) http.Handler {
+	api := &APIHandler{
+		DB:   db,
+		Game: game,
+	}
+
+	r := chi.NewRouter()
+
+	// Standalone GET Requests
+	r.Get("/news", api.getEvents) // Allow for query parameters
+
+	r.Get("/overview", api.getOverview) // Some stats for the dashboard
+	// Top 2 Gainers / Top 2 Loosers
+	// Maybe total + of all stock and things like that in the future
+
+	r.Get("/time", api.getTime) // Current time on the server
+
+	// API Routes
+	r.Route("/stocks", func(r chi.Router) {
+
+		r.Get("/", api.getAllStocks)
+
+		r.Get("/{symbol}", api.getStockDetails)
+
+		r.Post("/orders", api.addOrder)
+
+	})
+
+	r.Route("/orders", func(r chi.Router) {
+
+		r.Get("/", api.orders)
+
+		r.Get("/{orderID}", api.orders)
+
+		r.Delete("/{orderID}", api.orders)
+	})
+
+	r.Route("/portfolio", func(r chi.Router) {
+
+		r.Get("/", api.getPortfolio)
+
+		r.Get("/{symbol}", api.getPortfolio)
+	})
+
+	return r
+}
