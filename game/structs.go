d13b9ad1e078fe8b6fe6688be748769fd974135d
unknown <alexander.gemmingen@gmx.de>
2020-04-08 15:45:23 +0200

    Feature: Set up for event change dates
    Co-authored-by: juandogels <suwandresukijat@gmail.com>
    Co-authored-by: Henry Gressmann <mail@henrygressmann.de>

--- game/structs.go
+++ game/structs.go
@@ -30,8 +30,8 @@ type EventSettings struct {
 
 // TagOptions are more indepth settings for specific event tags only
 type TagOptions struct {
-	AffectsTag     string
-	AffectsStockID string
+	affectsTags     string
+	affectsStockIDs string
 	Trend          int64 // Note: .2 would be 20 and .02 would be 2
 	//// TimeOffset time.Duration // Optionally offset the event to e.g only affect a tag after x time
 	////Duration time.Duration
