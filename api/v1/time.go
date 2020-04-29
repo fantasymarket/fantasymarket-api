e71540aef2a73f37f6dd65ee2566f4bf50479dd0
Henry Gressmann <mail@henrygressmann.de>
2020-04-29 17:22:46 +0200

    feat: orders api (#20)
    * few amendments to the api
    
    * fixed stockDetails api route, displays proper results now
    
    * Feature: EventComputation with Fixed event type
    
    Co-authored-by: juandogels <suwandresukijat@gmail.com>
    Co-authored-by: Henry Gressmann <mail@henrygressmann.de>
    Co-authored-by: Arthur <arthurpbh@gmail.com>
    
    * changed addOrder handler
    
    * continued working on eventComputation and Database
    
    Co-authored-by: Henry Gressmann <mail@henrygressmann.de>
    Co-authored-by: juandogels <suwandresukijat@gmail.com>
    Co-authored-by: Arthur <arthurpbh@gmail.com>
    
    * Feat: Created Event Hist. Created Descr year swap
    
    Co-authored-by: Henry Gressmann <mail@henrygressmann.de>
    Co-authored-by: juandogels <suwandresukijat@gmail.com>
    
    * before breaking change
    
    * Fixed some Logic - CPU at 100% need help
    
    Co-authored-by: Arthur <arthurpbh@gmail.com>
    Co-authored-by: Henry Gressmann <mail@henrygressmann.de>
    Co-authored-by: juandogels <suwandresukijat@gmail.com>
    
    * Feat: added functions GetOrder and GetOrderByID. Check limits for ID as not implemented
    
    * Some fixing on the data base file
    
    Co-authored-by: juandogels <suwandresukijat@gmail.com>
    Co-authored-by: Henry Gressmann <mail@henrygressmann.de>
    Co-authored-by: Arthur <arthurpbh@gmail.com>
    
    * more fixes
    
    * refactored api handler structure and handlers
    
    * fix stuff
    
    Signed-off-by: exploingcamera <mail@henrygressmann.de>
    
    * cleanup ;p
    
    Signed-off-by: exploingcamera <mail@henrygressmann.de>
    
    Co-authored-by: Arthlight <arthurpbh@gmail.com>
    Co-authored-by: unknown <alexander.gemmingen@gmx.de>
    Co-authored-by: juandogels <suwandresukijat@gmail.com>

---
+++ api/v1/time.go
@@ -0,0 +1,17 @@
+package v1
+
+import (
+	"fantasymarket/utils/http/responses"
+	"net/http"
+	"time"
+)
+
+func (api *APIHandler) getTime(w http.ResponseWriter, r *http.Request) {
+	t := api.Config.Game.StartDate
+
+	if !t.IsZero() {
+		responses.CustomResponse(w, t.Format(time.RFC3339), 200)
+	} else {
+		responses.ErrorResponse(w, http.StatusInternalServerError, "error getting date")
+	}
+}
