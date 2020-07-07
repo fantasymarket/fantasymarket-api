a3a10a50f1ab82abd2383808ef1d3eb0793247d5
Alyks1 <48106762+Alyks1@users.noreply.github.com>
2020-07-07 14:06:59 +0200

    fix: removing TODO comment
    Co-authored-by: Henry Gressmann <mail@henrygressmann.de>

--- database/database-users_test.go
+++ database/database-users_test.go
@@ -20,34 +20,3 @@ func (suite *DatabaseTestSuite) TestCreateGuest() {
 		assert.Equal(suite.T(), user.Username, testUser.Username)
 	}
 }
-
-// func (s *Service) CreateGuest() (*models.User, error) {
-
-// 	var username string
-// 	// we generate usenames until the username is unique so everyone
-// 	// starts of with a fresh account
-// 	for username == "" {
-// 		u := petname.Generate(3, "-")
-
-// 		if s.DB.Where("username = ?", u).Select("username").First(&models.User{}).RecordNotFound() {
-// 			username = u
-// 		}
-// 	}
-
-// 	user := models.User{
-// 		Username: username,
-// 		Portfolio: models.Portfolio{
-// 			Balance: initialBalance,
-// 		},
-// 	}
-
-// 	if err := s.DB.Create(&user).Error; err != nil {
-// 		return nil, err
-// 	}
-
-// 	return &models.User{
-// 		CreatedAt: user.CreatedAt,
-// 		UserID:    user.UserID,
-// 		Username:  user.Username,
-// 	}, nil
-// }
